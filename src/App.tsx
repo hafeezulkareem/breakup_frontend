c3c0497be690884578f7527fc6b080e1407447a2
Hafeezul Kareem Shaik <hafeezulkareem@gmail.com>
2021-04-05 15:27:26 +0530

    Remove App.css file

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import "./App.css";
 import { SignIn } from "./Authentication/components/SignIn";
 import { SignUp } from "./Authentication/components/SignUp";
 import { PageNotFound } from "./Common/components/PageNotFound";
