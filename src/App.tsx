a7d17d67ee81eaa10f8de8ce8e1b0dcb6da92a77
Hafeezul Kareem Shaik <hafeezulkareem@gmail.com>
2021-04-05 12:11:24 +0530

    Add react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import "./App.css";
 import { SignIn } from "./Authentication/components/SignIn";
 
 function App() {
-   return <SignIn />;
+   return (
+      <Router>
+         <Switch>
+            <Route path="/" component={SignIn} />
+         </Switch>
+      </Router>
+   );
 }
 
 export default App;
