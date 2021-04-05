eeb201a7f8682dad510c59ccf6a208a5aa370692
Hafeezul Kareem Shaik <hafeezulkareem@gmail.com>
2021-04-05 12:00:56 +0530

    Add SignIn component

---
+++ src/Authentication/components/SignIn/SignIn.tsx
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+
+import { colors } from "../../../Common/themes/colors";
+import Button from "../../../Common/components/Button";
+
+import {
+   FormContainer,
+   FormInput,
+   SignInPage,
+   SignUpLink,
+   SignUpLinkContainer,
+   SubmitButton,
+   Title,
+   TitleContainer,
+} from "./styledComponents";
+
+const SignIn = (props) => {
+   const [email, setEmail] = useState("");
+   const [password, setPassword] = useState("");
+
+   const onChangeEmail = (event) => {
+      setEmail(event.target.value);
+   };
+
+   const onChangePassword = (event) => {
+      setPassword(event.target.value);
+   };
+
+   const login = (event) => {
+      event.preventDefault();
+   };
+
+   return (
+      <SignInPage>
+         <FormContainer onSubmit={login}>
+            <TitleContainer>
+               <Title>Login</Title>
+            </TitleContainer>
+            <FormInput
+               value={email}
+               placeholder={"Email"}
+               onChange={onChangeEmail}
+               startIcon={"email"}
+               iconColor={colors.gray3}
+            />
+            <FormInput
+               value={password}
+               type={"password"}
+               placeholder={"Password"}
+               onChange={onChangePassword}
+               startIcon={"password"}
+               iconColor={colors.gray3}
+            />
+            <SubmitButton color={Button.colors.primary} onClick={login}>
+               Login
+            </SubmitButton>
+            <SignUpLinkContainer>
+               Don't have an account? <SignUpLink>Create One</SignUpLink>
+            </SignUpLinkContainer>
+         </FormContainer>
+      </SignInPage>
+   );
+};
+
+export { SignIn };
---
+++ src/Authentication/components/SignIn/styledComponents.ts
@@ -0,0 +1,57 @@
+import tw, { styled } from "twin.macro";
+
+import Input from "../../../Common/components/Input";
+import Button from "../../../Common/components/Button";
+
+export const SignInPage = styled.div`
+   ${tw`
+        flex flex-col justify-center items-center h-screen
+    `}
+`;
+
+export const TitleContainer = styled.div`
+   ${tw`
+        mb-16px
+    `}
+`;
+
+export const Title = styled.span`
+   ${tw`
+        text-lg
+    `}
+`;
+
+export const FormContainer = styled.form`
+   ${tw`
+        flex flex-col w-full md:w-1/5 border border-solid border-gray3 shadow rounded-16px py-32px px-28px
+    `}
+`;
+
+export const FormInput = styled(Input)`
+   ${tw`
+        w-full
+    `}
+`;
+
+export const SubmitButton = styled(Button)`
+   ${tw`
+        w-full mt-8px border border-solid border-2 bg-royalBlue
+    `}
+   &:focus {
+      ${tw`
+            border-blue-800
+        `}
+   }
+`;
+
+export const SignUpLinkContainer = styled.span`
+   ${tw`
+        text-gray3 text-xs mt-16px mx-auto
+    `}
+`;
+
+export const SignUpLink = styled.span`
+   ${tw`
+        text-persianBlue cursor-pointer
+    `}
+`;
