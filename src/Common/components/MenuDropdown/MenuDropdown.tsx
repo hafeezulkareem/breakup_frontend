39620d8f7aa67e99cb277aaf38276271990204fe
Anil <peddireddyanilchowdary@gmail.com>
2021-04-09 11:31:23 +0530

    Add Profile and Logout features

---
+++ src/Common/components/MenuDropdown/MenuDropdown.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { MdExpandMore } from "react-icons/md";
+import { useHistory } from "react-router-dom";
+
+import { clearUserSession } from "../../utils/StorageUtils";
+
+import BaseDropdown from "./../BaseDropdown";
+import { DropDownButton, Menu, MenuItem } from "./styledComponents";
+
+interface MenuDropdownProps {
+	user?: any;
+}
+const MenuDropdown = (props: MenuDropdownProps) => {
+	const history = useHistory();
+
+	const logout = () => {
+		clearUserSession();
+		history.replace("/sign-in");
+	};
+
+	return (
+		<BaseDropdown>
+			{(onTrigger, show) => (
+				<>
+					<DropDownButton onClick={() => onTrigger()}>
+						<h4 className='ml-2 md:ml-4 mr-2 font-bold'>Anil Kumar</h4>
+						<MdExpandMore />
+					</DropDownButton>
+					{show && (
+						<Menu>
+							<MenuItem onClick={logout}>Logout</MenuItem>
+						</Menu>
+					)}
+				</>
+			)}
+		</BaseDropdown>
+	);
+};
+
+export default MenuDropdown;
--- src/Dashboard/components/Header/Header.tsx
+++ src/Dashboard/components/Header/Header.tsx
@@ -2,45 +2,63 @@ import React, { ReactNode } from "react";
 import { observer } from "mobx-react-lite";
 import { FcGenealogy } from "react-icons/fc";
 
+import MenuDropdown from "../../../Common/components/MenuDropdown/MenuDropdown";
+import UserModel from "../../../Authentication/stores/models/UserModel/ index";
+
 import {
-   HeaderContainer,
-   LeftContainer,
-   LogoContainer,
-   Title,
-   TitleAndLogoContainer,
-   TitleAndNavigationContainer,
+	HeaderContainer,
+	LeftContainer,
+	LogoContainer,
+	Profile,
+	ProfileContainer,
+	RightContainer,
+	Title,
+	TitleAndLogoContainer,
+	TitleAndNavigationContainer,
 } from "./styledComponents";
 
 interface HeaderProps {
-   titleAndNavigation?: () => ReactNode;
+	titleAndNavigation?: () => ReactNode;
+	user?: UserModel;
 }
 
 const Header = observer((props: HeaderProps) => {
-   const { titleAndNavigation } = props;
-   const renderTitleAndNavigation = (): ReactNode => {
-      if (titleAndNavigation) {
-         return (
-            <TitleAndNavigationContainer>
-               {titleAndNavigation()}
-            </TitleAndNavigationContainer>
-         );
-      }
-      return null;
-   };
+	const { titleAndNavigation } = props;
+	const renderTitleAndNavigation = (): ReactNode => {
+		if (titleAndNavigation) {
+			return (
+				<TitleAndNavigationContainer>
+					{titleAndNavigation()}
+				</TitleAndNavigationContainer>
+			);
+		}
+		return null;
+	};
 
-   return (
-      <HeaderContainer>
-         <LeftContainer>
-            <TitleAndLogoContainer>
-               <LogoContainer>
-                  <FcGenealogy size={42} />
-               </LogoContainer>
-               <Title>BreakUp</Title>
-            </TitleAndLogoContainer>
-            {renderTitleAndNavigation()}
-         </LeftContainer>
-      </HeaderContainer>
-   );
+	return (
+		<HeaderContainer>
+			<LeftContainer>
+				<TitleAndLogoContainer>
+					<LogoContainer>
+						<FcGenealogy size={42} />
+					</LogoContainer>
+					<Title>BreakUp</Title>
+				</TitleAndLogoContainer>
+				{renderTitleAndNavigation()}
+			</LeftContainer>
+			<RightContainer>
+				<ProfileContainer>
+					<Profile
+						src='https://res.cloudinary.com/trucmachin/image/upload/c_thumb,w_400,g_face/v1607022210/avatar/spwho5a72zebv7wjfdvu.jpg'
+						className='w-full'
+						alt='avatar'
+					/>
+				</ProfileContainer>
+				{/* <Username>Anil Kumar</Username> */}
+				<MenuDropdown />
+			</RightContainer>
+		</HeaderContainer>
+	);
 });
 
 export { Header };
