7b5a37e29a335859ba21021588de362f78b7ef7f
Hafeezul Kareem Shaik <hafeezulkareem@gmail.com>
2021-05-20 12:48:14 +0530

    Update user details

--- src/Common/components/MenuDropdown/MenuDropdown.tsx
+++ src/Common/components/MenuDropdown/MenuDropdown.tsx
@@ -8,33 +8,34 @@ import BaseDropdown from "./../BaseDropdown";
 import { DropDownButton, Menu, MenuItem } from "./styledComponents";
 
 interface MenuDropdownProps {
-	user?: any;
+   user?: string;
 }
 const MenuDropdown = (props: MenuDropdownProps) => {
-	const history = useHistory();
+   const { user } = props;
+   const history = useHistory();
 
-	const logout = () => {
-		clearUserSession();
-		history.replace("/sign-in");
-	};
+   const logout = () => {
+      clearUserSession();
+      history.replace("/sign-in");
+   };
 
-	return (
-		<BaseDropdown>
-			{(onTrigger, show) => (
-				<>
-					<DropDownButton onClick={() => onTrigger()}>
-						<h4 className='ml-2 md:ml-4 mr-2 font-bold'>Anil Kumar</h4>
-						<MdExpandMore />
-					</DropDownButton>
-					{show && (
-						<Menu>
-							<MenuItem onClick={logout}>Logout</MenuItem>
-						</Menu>
-					)}
-				</>
-			)}
-		</BaseDropdown>
-	);
+   return (
+      <BaseDropdown>
+         {(onTrigger, show) => (
+            <>
+               <DropDownButton onClick={() => onTrigger()}>
+                  <h4 className="ml-2 md:ml-4 mr-2 font-bold">{user}</h4>
+                  <MdExpandMore />
+               </DropDownButton>
+               {show && (
+                  <Menu>
+                     <MenuItem onClick={logout}>Logout</MenuItem>
+                  </Menu>
+               )}
+            </>
+         )}
+      </BaseDropdown>
+   );
 };
 
 export default MenuDropdown;
--- src/Dashboard/components/Header/Header.tsx
+++ src/Dashboard/components/Header/Header.tsx
@@ -3,62 +3,70 @@ import { observer } from "mobx-react-lite";
 import { FcGenealogy } from "react-icons/fc";
 
 import MenuDropdown from "../../../Common/components/MenuDropdown/MenuDropdown";
-import { UserModel } from "../../../Authentication/stores/models/UserModel";
+import { useStores } from "../../../Common/stores";
 
 import {
-	HeaderContainer,
-	LeftContainer,
-	LogoContainer,
-	Profile,
-	ProfileContainer,
-	RightContainer,
-	Title,
-	TitleAndLogoContainer,
-	TitleAndNavigationContainer,
+   HeaderContainer,
+   LeftContainer,
+   LogoContainer,
+   Profile,
+   ProfileContainer,
+   RightContainer,
+   Title,
+   TitleAndLogoContainer,
+   TitleAndNavigationContainer,
 } from "./styledComponents";
 
 interface HeaderProps {
-	titleAndNavigation?: () => ReactNode;
-	user?: UserModel;
+   titleAndNavigation?: () => ReactNode;
 }
 
 const Header = observer((props: HeaderProps) => {
-	const { titleAndNavigation } = props;
-	const renderTitleAndNavigation = (): ReactNode => {
-		if (titleAndNavigation) {
-			return (
-				<TitleAndNavigationContainer>
-					{titleAndNavigation()}
-				</TitleAndNavigationContainer>
-			);
-		}
-		return null;
-	};
+   const { titleAndNavigation } = props;
 
-	return (
-		<HeaderContainer>
-			<LeftContainer>
-				<TitleAndLogoContainer>
-					<LogoContainer>
-						<FcGenealogy size={42} />
-					</LogoContainer>
-					<Title>BreakUp</Title>
-				</TitleAndLogoContainer>
-				{renderTitleAndNavigation()}
-			</LeftContainer>
-			<RightContainer>
-				<ProfileContainer>
-					<Profile
-						src='https://res.cloudinary.com/trucmachin/image/upload/c_thumb,w_400,g_face/v1607022210/avatar/spwho5a72zebv7wjfdvu.jpg'
-						className='w-full'
-						alt='avatar'
-					/>
-				</ProfileContainer>
-				{/* <Username>Anil Kumar</Username> */}
-				<MenuDropdown />
-			</RightContainer>
-		</HeaderContainer>
-	);
+   const {
+      authStore: { user },
+   } = useStores();
+
+   let name = "";
+   if (user) {
+      name = user.name;
+   }
+
+   const renderTitleAndNavigation = (): ReactNode => {
+      if (titleAndNavigation) {
+         return (
+            <TitleAndNavigationContainer>
+               {titleAndNavigation()}
+            </TitleAndNavigationContainer>
+         );
+      }
+      return null;
+   };
+
+   return (
+      <HeaderContainer>
+         <LeftContainer>
+            <TitleAndLogoContainer>
+               <LogoContainer>
+                  <FcGenealogy size={42} />
+               </LogoContainer>
+               <Title>BreakUp</Title>
+            </TitleAndLogoContainer>
+            {renderTitleAndNavigation()}
+         </LeftContainer>
+         <RightContainer>
+            <ProfileContainer>
+               <Profile
+                  src="https://res.cloudinary.com/trucmachin/image/upload/c_thumb,w_400,g_face/v1607022210/avatar/spwho5a72zebv7wjfdvu.jpg"
+                  className="w-full"
+                  alt="avatar"
+               />
+            </ProfileContainer>
+            <MenuDropdown user={name} />
+         </RightContainer>
+      </HeaderContainer>
+   );
 });
 
 export { Header };
